3461052d6c987e9f26bc61a6ff4a6f68633e636f
vitorjr <vitorufc2013@gmail.com>
2021-01-24 11:20:00 -0300

    Adicionando Model StockResources e inserindo Frameworks necessarios

---
+++ ZombieResources/ZombieResources/Models/StockResources.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZombieResources.Models
+{
+    [Table("StockResources")]
+    public class StockResources
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Description")]
+        public string Description { get; set; }
+
+        [Column("Observation")]
+        public string Observation { get; set; }
+        [Column("Amount")]
+        public float Amout { get; set; }
+        [Column("Responsibleinput")]
+        public string Responsibleinput { get; set; }
+        [Column("Responsibleoutput")]
+        public string Responsibleoutput { get; set; }
+    }
+}
